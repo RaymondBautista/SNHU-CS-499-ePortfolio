cb46f7bd71441584cd22225bda31466b633b6429
RaymondBautista <raymondbautista296@gmail.com>
2026-02-21 21:34:15 -0400

    Implemented EventRepository and updated AppDatabase

--- app/src/main/java/com/snhu/events/data/AppDatabase.java
+++ app/src/main/java/com/snhu/events/data/AppDatabase.java
@@ -6,8 +6,8 @@
  * Creates the physical SQLite internal database file
  * on the device using the Singleton pattern.
  *
- * Last Modified: 2026-02-08
- * Version: 1.0.0
+ * Last Modified: 2026-02-21
+ * Version: 2.0.0
  *
  * Author: Raymond Bautista
  */
@@ -17,18 +17,21 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import com.snhu.events.model.Event;
 import com.snhu.events.model.User;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, Event.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase instance;
+
     public abstract UserDao userDao();
+    public abstract EventDao eventDao(); // Add this line
 
     public static synchronized AppDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                             AppDatabase.class, "events_database")
-                    .fallbackToDestructiveMigration()
+                    .fallbackToDestructiveMigration() // Note: This clears DB on version change
                     .build();
         }
         return instance;
